51af7b500521aebf5fcfbc4c1466e4468a5ef4e7
Shaunak <shaunaksompura@gmail.com>
2019-12-23 04:53:44 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {      //app- Express app to register this route handler with
-  res.send({ hi: 'there' });      //'/' - Watch for requests trying to access '/'
+  res.send({ hiye: 'byee' });      //'/' - Watch for requests trying to access '/'
 })                                // req - Object representing incoming request
 
 const PORT = process.env.PORT || 5000;    //For Heroku (prod env) || for development environment
