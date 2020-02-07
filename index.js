4aaf5b4078bea6c3da16d1601586446dc190ed27
Shaunak <shaunaksompura@gmail.com>
2020-02-07 16:48:51 -0500

    commit

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {      //app- Express app to register this route handler with
-  res.send({ hiye: 'byee' });      //'/' - Watch for requests trying to access '/'
-})                                // req - Object representing incoming request
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
+}
+)
+);
+
+app.get('/auth/google',
+passport.authenticate('google',{
+  scope: ['profile','email']
+})
+);
+
 
 const PORT = process.env.PORT || 5000;    //For Heroku (prod env) || for development environment
-                                  // res - Object " " outgoing response
-app.listen(PORT);                 // res.send - Send some JSON back
-                                  //Express telling node to listen to port 5000
+
+app.listen(PORT);                 //Express telling node to listen to port 5000
 
                                   // localhost:5000
