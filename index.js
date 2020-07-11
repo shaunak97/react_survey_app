78d510a6d337bd65ae586851bf8d6ac3a1314dd5
Shaunak <shaunaksompura@gmail.com>
2020-07-11 19:08:46 -0400

    prod

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+/*
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); // gives access to cookies
+const passport = require('./config/keys'); //tells express to use cookies in application
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -9,6 +12,17 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //cookie lasts for 30 days in browser
+    keys: [keys.cookieKey] //used to encrypt cookie
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 require('./routes/authRoutes')(app);
 
 
@@ -17,3 +31,34 @@ const PORT = process.env.PORT || 5000;    //For Heroku (prod env) || for develop
 app.listen(PORT);                 //Express telling node to listen to port 5000
 
                                   // localhost:5000
+
+
+*/
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+                  
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,22 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => { //user is mongoose model instance and transformed into id which is passed on with the cookie
+  done(null, user.id); //user id is the id given by mongo to a record
+                      //mongo id is used to be consistent throughout all signing in methods
+                      //After sign in profile id (Google id) does not come in play
+
+});
+
+passport.deserializeUser((id, done) => {
+  //id is taken and transformed into mongoose model instance
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+  //Pass in the id of the record we want to find
+  //Async action, returns a promise
+});
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
@@ -12,6 +28,7 @@ passport.use(new GoogleStrategy({
 },
 
  (accessToken, refreshToken, profile, done) => {
+   //findOne searches DB to see if a record exists
    User.findOne({ googleId: profile.id }).then((existingUser) => {
         if(existingUser){
           // we already have a record with the given profile // ID
