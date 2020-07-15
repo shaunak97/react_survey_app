48faa62ecde2968b91d1f302aae41cf46bf639db
Shaunak <shaunaksompura@gmail.com>
2020-07-14 23:38:02 -0400

    concurrent servers

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        My react app
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="http://localhost:5000/auth/google"> Sign In With Google </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
