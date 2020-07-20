8a154538a8f2242bcc45c8c9b23bca589c1abd5d
Shaunak <shaunaksompura@gmail.com>
2020-07-20 17:10:09 -0400

    production deployment

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser'); //Express middleware that operates on the incoming requests
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if( process.env.NODE_ENV === 'production' ){
+  //Express will serve up production assets
+  //Like our main.js file or main.css file
+  app.use(express.static('client/build'));
+
+  //Express will serve up the index.html file
+  //If it doesn't recognize the file
+  const path = require('path');
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+
+
 /*
 const express = require('express');
 const mongoose = require('mongoose');
@@ -34,33 +79,3 @@ app.listen(PORT);                 //Express telling node to listen to port 5000
 
 
 */
-
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser'); //Express middleware that operates on the incoming requests
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
-
-mongoose.connect(keys.mongoURI);
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
