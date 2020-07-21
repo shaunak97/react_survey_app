09ac840aac233adfa35b5590ba38ea7c724eb56b
Shaunak <shaunaksompura@gmail.com>
2020-07-21 14:13:51 -0400

    Survey creation

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [RecipientSchema], //Sub document collection
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: { type: Schmea.Types.ObjectId, ref: 'User' }, //It is a relationship field between a given schema and a User
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
