1ed61de54a8af347471b030226603414917592ab
Shaunak <shaunaksompura@gmail.com>
2020-07-21 22:44:47 -0400

    Sendgrid Integration

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,16 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
 
+    this.from_email = new helper.Email('1997shaunak@gmail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
 }
+
+new Mailer(...)
 
 module.exports = Mailer;
