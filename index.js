29c657ffa8c883f2c090705affe7f5abe579ed8d
Shaunak <shaunaksompura@gmail.com>
2020-08-10 22:25:05 -0400

    Send grid module ends

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
+
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -25,6 +26,8 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
+
 
 if( process.env.NODE_ENV === 'production' ){
   //Express will serve up production assets
